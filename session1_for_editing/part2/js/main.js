20c4bfef8f7160ae6d577e782486509cbdf2522f
kzya <nagatomo10311031@gmail.com>
2014-11-14 17:24:51 +0900

    editing2を変更しました

--- session1_for_editing/part2/js/main.js
+++ session1_for_editing/part2/js/main.js
@@ -1,4 +1,4 @@
-// ecnhant.jsを初期化
+/// ecnhant.jsを初期化
 enchant();
 
 window.onload = function() {
@@ -9,6 +9,13 @@ window.onload = function() {
   // ゲームが動作開始した時のコード
 	game.onload = function() {
 
+		// 文字を表示するにはLabelオブジェクト
+		hello = new Label("Hello world.");
+		hello.x = 100;	// 画面左上を起点として横の位置
+		hello.y = 100;	// 画面左上を起点として縦の位置
+
+		// rootSceneの下に追加
+		game.rootScene.addChild(hello);
   }
 
   // ゲーム開始
